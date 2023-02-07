15a1e2370655c758f9613abc3c88105bff48e90b
Colin <colins110@hotmail.com>
2023-02-07 14:22:05 +0800

    Update wsl2_clashConfig.sh

--- wsl2_clashConfig.sh
+++ wsl2_clashConfig.sh
@@ -5,11 +5,11 @@ shellName=`echo $SHELL | awk -F "/" '{print $NF}'`
 if [ $shellName == 'bash' ]
 then
     echo 'hostip=$(cat /etc/resolv.conf |grep -oP "(?<=nameserver\ ).*")' >> ~/.bashrc
-    echo 'setp="export https_proxy=http://${hostip}:7890;export http_proxy=http://${hostip}:7890;export all_proxy=socks5://${hostip}:7890;export ALL_PROXY=socks5://${hostip}:7890;"' >> ~/.bashrc
+    echo 'alias setp="export https_proxy=http://${hostip}:7890;export http_proxy=http://${hostip}:7890;export all_proxy=socks5://${hostip}:7890;export ALL_PROXY=socks5://${hostip}:7890;"' >> ~/.bashrc
     echo 'alias unsetp="unset https_proxy; unset http_proxy; unset all_proxy; unset ALL_PROXY;"' >> ~/.bashrc
 elif [ $shellName == 'zsh' ]
 then
     echo 'hostip=$(cat /etc/resolv.conf |grep -oP "(?<=nameserver\ ).*")' >> ~/.zshrc
-    echo 'setp="export https_proxy=http://${hostip}:7890;export http_proxy=http://${hostip}:7890;export all_proxy=socks5://${hostip}:7890;export ALL_PROXY=socks5://${hostip}:7890;"' >> ~/.zshrc
+    echo 'alias setp="export https_proxy=http://${hostip}:7890;export http_proxy=http://${hostip}:7890;export all_proxy=socks5://${hostip}:7890;export ALL_PROXY=socks5://${hostip}:7890;"' >> ~/.zshrc
     echo 'alias unsetp="unset https_proxy; unset http_proxy; unset all_proxy; unset ALL_PROXY;"' >> ~/.zshrc
 fi
